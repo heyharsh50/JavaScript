0550203ee3ecbae0adee6852e4297d9a389c7e56
Harsh Raj <harshraj88253@gmail.com>
2024-09-19 15:31:19 +0000

    Basics concept of const, let and var.

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,16 @@
 console.log("Harsh Raj")
+
+const accountId = 202100129
+let accountEmail = "harsh@gmail.com"
+var accountPassword = "123456"
+accountCity = "Motihari"
+let accountStatet  //If we declare and not assign any value to that it will show undefined
+
+// accountID = 2 not allowed because it's constant
+
+accountEmail = "xyz@gmail.com"
+accountPassword = "696969"
+accountCity = "Patna"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountStatet]);
