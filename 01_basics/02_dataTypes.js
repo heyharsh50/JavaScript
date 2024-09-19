cdf9bcb82092e509823cda3cf02c0431ad72ca1c
Harsh Raj <harshraj88253@gmail.com>
2024-09-19 17:09:48 +0000

    variables and dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) we are using nodejs, not browser
+
+// console.log(3+3); console.log("3+3"); without semicolon it will throw error and after we use semicolon it works but not a good practice and bad readability
+
+let name = "Harsh" //string dataType
+let age = 21 //number dataType
+let isLoggedIn = true //boolean dataType
+
+ console.table([name, age, isLoggedIn]);
+
+ //console.log(name, age, isLoggedIn); print in same line
+
+// number -> 2 to power 53 
+// string -> ""
+// bigint
+// boolean -> true/false
+// null -> standalone value (empty value) eg. let state = null
+// undefined -> eg. let state; (variable declared but value not assigned)
+// symbol -> unique 
+
+// object
+
+console.log(typeof "Harsh");
+console.log(typeof 3);
+
+console.log(typeof undefined);      // output -> undefined
+console.log(typeof null);          // output -> object
+
